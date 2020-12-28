9cfbd544985b77522edf0c2ff7d0546bed85e385
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-12-28 15:56:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo """
 Menu :
   (1) Install Bazel & Install OpenCV  & Copmile Hand-Counting App
   (2) Run Hand-Counting App
-  
+  Enter 1 or 2 >:
 """
 read Choice
 
@@ -20,7 +20,7 @@ if [ $Choice == "1" ]; then
         mkdir $HOME/bazel-3.4.1
         cd $HOME/bazel-3.4.1
         wget https://github.com/bazelbuild/bazel/releases/download/3.4.1/bazel-3.4.1-dist.zip
-        sudo apt-get install build-essential openjdk-8-jdk python zip unzip
+        sudo apt-get install build-essential openjdk-8-jdk python zip unzip -y
         unzip bazel-3.4.1-dist.zip
         env EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk" bash ./compile.sh
         sudo cp output/bazel /usr/local/bin/
@@ -31,16 +31,16 @@ if [ $Choice == "1" ]; then
     
     echo "** Change Libary Path **" ; sed -i "s/x86_64-linux-gnu/aarch64-linux-gnu/g" third_party/opencv_linux.BUILD
 
-    echo "** Cloning MediaPipe Repo **" ; https://github.com/dspip/Hand_detector.git /$HOME
+    echo "** Cloning Hand_detector Repo **" ; https://github.com/dspip/Hand_detector.git /$HOME
     cd /$HOME/Hand_detector ; sudo bash ./setup_opencv.sh
 
 
     echo '** Copmile Hand-Counting App **'
     cd /$HOME/Hand_detector ; bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 mediapipe/examples/desktop/hand_tracking:hand_tracking_out_gpu --verbose_failures
 
-fi
 
 elif [ $Choice == "2" ]; then
+
     echo '** Hand-Counting App **'
     cd /$HOME/Hand_detector ; GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_out_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_desktop_live_gpu.pbtxt
 
