e9b66a69877af27fbd1e20b06cd34ad1c973cd52
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-01-06 10:04:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,23 +27,20 @@ if [ $Choice == "1" ]; then
         sudo cp output/bazel /usr/local/bin/
         sleep 2
         echo "** Bazel installed Successfully **"
-
     fi
     
     echo "** Change Libary Path **" ; sed -i "s/x86_64-linux-gnu/aarch64-linux-gnu/g" third_party/opencv_linux.BUILD
-
-    echo "** Cloning Hand_detector Repo **" ; cd $HOME && sudo git clone https://github.com/dspip/Hand_detector.git
     sudo apt autoremove -y
-    cd $HOME/Hand_detector ; sudo bash ./setup_opencv.sh
+    sudo bash ./setup_opencv.sh
 
 
     echo '** Copmile Hand-Counting App **'
-    cd $HOME/Hand_detector ; bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 mediapipe/examples/desktop/hand_tracking:hand_tracking_out_gpu --verbose_failures
+    bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 mediapipe/examples/desktop/hand_tracking:hand_tracking_out_gpu --verbose_failures
 
 
 elif [ $Choice == "2" ]; then
 
     echo '** Hand-Counting App **'
-    cd $HOME/Hand_detector ; GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_out_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_desktop_live_gpu.pbtxt
+    GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_out_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_desktop_live_gpu.pbtxt
 
 fi
