fe2dbe1531f4d26a5b3e8f64b6444e21106b5237
Shvarpa <shvarpa@gmail.com>
2021-02-17 15:05:41 +0200

    Add run_docker script

---
+++ run_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Pull The image"
+sudo docker pull 247555/hand_tracker:latest
+xhost +
+
+
+echo -e "Make sure the device is video0 and video1 ! \nAnd Chaneg and MQTT IP"
+
+sudo docker run --rm -d --network="host" --gpus all --device=/dev/video0:/dev/video0 --runtime nvidia -e MQTT="mqtt://localhost:1883" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix hand_tracker &
+sudo docker run --rm -d --network="host" --gpus all --device=/dev/video1:/dev/video0 --runtime nvidia -e MQTT="mqtt://localhost:1883" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix hand_tracker
