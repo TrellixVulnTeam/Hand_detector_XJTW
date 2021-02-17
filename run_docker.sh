219f2665a96671653e3a48b71ebbc119a1d69601
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-02-17 15:21:47 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ sudo docker pull 247555/hand_tracker:latest
 xhost +
 
 
-echo -e " **** Make sure the device is video0 and video1 ! \nAnd Chaneg and MQTT IP ****"
+echo -e " **** Make sure the device is video0 and video1 ! \nAnd Chaneg The MQTT IP ****"
 
 sudo docker run --rm -d --network="host" --gpus all --device=/dev/video0:/dev/video0 --runtime nvidia -e MQTT="mqtt://localhost:1883" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix hand_tracker &
 sudo docker run --rm -d --network="host" --gpus all --device=/dev/video1:/dev/video0 --runtime nvidia -e MQTT="mqtt://localhost:1883" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix hand_tracker
